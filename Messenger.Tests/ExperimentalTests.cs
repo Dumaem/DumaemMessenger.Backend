d7390d7c175faeba243fc1d01e6cfb70b393c7e7
Камиль Шарипов <wingimobile@gmail.com>
2023-01-01 19:53:16 +0300

    Add unsuccessful test

--- Messenger.Tests/ExperimentalTests.cs
+++ Messenger.Tests/ExperimentalTests.cs
@@ -1,5 +1,12 @@
+using FluentAssertions;
+
 namespace Messenger.Tests;
 
 public class ExperimentalTests
 {
+    [Fact]
+    public void UnsuccessfulTest()
+    {
+        10.Should().Be(12);
+    }
 }
