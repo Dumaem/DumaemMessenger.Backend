d8aa54752bf267e84916312641c39c46617ad290
Камиль Шарипов <wingimobile@gmail.com>
2023-01-01 13:13:53 +0300

    Delete experimental tests from class

--- Messenger.Tests/ExperimentalTests.cs
+++ Messenger.Tests/ExperimentalTests.cs
@@ -1,14 +1,5 @@
-using FluentAssertions;
-
 namespace Messenger.Tests;
 
 public class ExperimentalTests
 {
-    [Fact]
-    public void ExperimentalTest_ShouldSuccess()
-    {
-        var result = 10;
-
-        result.Should().Be(10);
-    }
 }
