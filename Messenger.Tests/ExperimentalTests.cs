5cdccb668b08504a99cf5ddbae09c25f6a041481
Камиль Шарипов <wingimobile@gmail.com>
2023-01-01 13:17:56 +0300

    Remove unused tests

--- Messenger.Tests/ExperimentalTests.cs
+++ Messenger.Tests/ExperimentalTests.cs
@@ -1,22 +1,5 @@
-using FluentAssertions;
-
 namespace Messenger.Tests;
 
 public class ExperimentalTests
 {
-    [Fact]
-    public void ExperimentalTest_ShouldSuccess()
-    {
-        var result = 10;
-
-        result.Should().Be(10);
-    }
-    
-    [Fact]
-    public void ExperimentalTest_ShouldFail()
-    {
-        var result = 10;
-
-        result.Should().Be(12);
-    }
 }
