f5db6672b43490c4bb2ef3e878968740d60c856f
Камиль Шарипов <wingimobile@gmail.com>
2023-01-01 13:06:21 +0300

    Remove experimental tests

--- Messenger.Tests/ExperimentalTests.cs
+++ Messenger.Tests/ExperimentalTests.cs
@@ -7,16 +7,5 @@ public class ExperimentalTests
     [Fact]
     public void ExperimentalTest_ShouldSuccess()
     {
-        var result = 10;
-
-        result.Should().Be(10);
-    }
-    
-    [Fact]
-    public void ExperimentalTest_ShouldFail()
-    {
-        var result = 10;
-
-        result.Should().Be(12);
     }
 }
