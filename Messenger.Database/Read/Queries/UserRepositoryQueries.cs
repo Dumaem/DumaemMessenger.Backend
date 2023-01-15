37d4f4952c8182a9741af8416bac824955588323
Flictivity <bulat_sa1@mail.ru>
2023-01-15 21:35:06 +0300

    change user repository query

--- Messenger.Database/Read/Queries/UserRepositoryQueries.cs
+++ Messenger.Database/Read/Queries/UserRepositoryQueries.cs
@@ -3,6 +3,6 @@
 internal static class UserRepositoryQueries
 {
     internal const string GetUserByEmail = "SELECT * FROM public.user WHERE email = @email";
-    internal const string GetUserById = "SELECT * FROM public.user WHERE email = @id";
+    internal const string GetUserById = "SELECT * FROM public.user WHERE id = @id";
     internal const string GetUserEncryptedPassword = "SELECT password FROM public.user WHERE id = @id";
 }
