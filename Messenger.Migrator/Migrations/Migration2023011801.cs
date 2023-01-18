064e02206d591deab97689fa776e3855b3b1faf1
Камиль Шарипов <wingimobile@gmail.com>
2023-01-18 21:32:04 +0300

    Add unique indexes on email and username

---
+++ Messenger.Migrator/Migrations/Migration2023011801.cs
@@ -0,0 +1,22 @@
+﻿namespace Messenger.Migrator.Migrations;
+
+[Migration(2023011801)]
+public class Migration2023011801 : Migration
+{
+    public override void Up()
+    {
+        Create
+            .UniqueConstraint()
+            .OnTable("user")
+            .Column("email");
+        Create
+            .UniqueConstraint()
+            .OnTable("user")
+            .Column("username");
+    }
+
+    public override void Down()
+    {
+        throw new NotImplementedException();
+    }
+}
