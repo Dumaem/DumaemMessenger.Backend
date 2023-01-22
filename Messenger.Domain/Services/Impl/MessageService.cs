8e237e71efbfa7407caec4b4deb2d1b5ba1ccdf3
riazonbin <riazonbin@mail.ru>
2023-01-22 19:13:02 +0300

    another little update

--- Messenger.Domain/Services/Impl/MessageService.cs
+++ Messenger.Domain/Services/Impl/MessageService.cs
@@ -91,7 +91,7 @@ namespace Messenger.Domain.Services.Impl
 
         public async Task<bool> EditMessageAsync(long messageId, byte[] content, int messageTypeId)
         {
-            var message = await _messageRepository?.GetMessageByIdAsync(messageId);
+            var message = await _messageRepository.GetMessageByIdAsync(messageId);
             // Use the repository to update the message's content
             message.Content = new MessageContent { Content = content, TypeId = messageTypeId };
             await _messageRepository.EditMessageByIdAsync(messageId, message);
