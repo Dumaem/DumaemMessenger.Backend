ae12ecc0a9862f02219dddf2007cbcb7c46eaa7a
Kamil Sharipov <wingimobile@gmail.com>
2023-02-07 15:04:31 +0300

    add connection to migration class

--- Messenger.Migrator/MessengerMigration.cs
+++ Messenger.Migrator/MessengerMigration.cs
@@ -1,9 +1,17 @@
 ﻿using FluentMigrator;
+using Npgsql;
 
 namespace Messenger.Migrator;
 
 public abstract class MessengerMigration : Migration
 {
+    protected NpgsqlConnection Connection;
+
+    protected MessengerMigration()
+    {
+        Connection = new(ConnectionString);
+    }
+
     public override void Down()
     {
         throw new NotSupportedException();
