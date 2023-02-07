6488787d88edcd8fb56e97b6841b3d2279a7641a
Flictivity <bulat_sa1@mail.ru>
2023-02-07 16:04:13 +0300

    added migration for validation table

---
+++ Messenger.Migrator/Migrations/Migration2023070201.cs
@@ -0,0 +1,29 @@
+﻿using FluentMigrator;
+
+namespace Messenger.Migrator.Migrations
+{
+    [Migration(2023070201)]
+    public class Migration2023070201 : Migration
+    {
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Up()
+        {
+            Alter.Column("is_verified")
+                .OnTable("user")
+                .AsBoolean()
+                .WithDefaultValue(false);
+            Create.Table("verification")
+                .WithColumn("id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("token").AsString(250)
+                .WithColumn("expiry_date").AsCustom("timestamp with time zone")
+                .WithColumn("user_id").AsInt32().ForeignKey("user", "id");
+            Create.UniqueConstraint("uniqToken")
+                .OnTable("verification")
+                .Columns("token", "user_id");
+        }
+    }
+}
